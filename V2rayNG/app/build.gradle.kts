52834ee47ee5303553135ff13cc34009473d8e0e
vatandev <vatandev@users.noreply.github.com>
2026-05-15 12:50:31 -0700

    fix icon

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.v2ray.ang.pro"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.0.4"
+        versionCode = 5
+        versionName = "0.0.5"
         multiDexEnabled = true
 
         val abiFilterList = (properties["ABI_FILTERS"] as? String)?.split(';')
